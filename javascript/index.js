3488f6bdbea6b15dcc3d40dcbc1b159c5c257ecf
Joey <joey.s.hornsby@gmail.com>
2021-04-29 13:36:04 +0100

    portfolio v1

--- javascript/index.js
+++ javascript/index.js
@@ -5,51 +5,74 @@ let box3Header = "BANK TECH TEST";
 let box4Header = "BOWLING CHALLENGE";
 let box5Header = "NOTES";
 
-let box1Content;
-let box2Content;
-let box3Content;
-let box4Content;
-let box5Content;
+let box1Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+let box2Content = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+let box3Content = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+let box4Content = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+let box5Content = "+1 synth sustainable duis la croix glossier, sint freegan pitchfork williamsburg craft beer esse affogato. Polaroid biodiesel tilde eu seitan.";
+
+$('a').each(function() {
+      $(this).click(function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          window.open(this.href, '_blank');
+      });
+  });
 
 
-  document.getElementById("box1").addEventListener('mouseover', function(){
-    first.innerHTML = "Think outside the box. Could you try something new? What can you do with what you have? Could you substitute one ingredient for another to mix things up? Don't be afraid to experiment!";
-    first.classList.add('para');
-  });
-  document.getElementById("box1").addEventListener('mouseout', function(){
-      first.innerHTML = box1Header;
-      first.classList.remove('para');
-  });
-    document.getElementById("box2").addEventListener('mouseover', function(){
-      second.innerHTML = "Don't feel the need to create something extravagant, or with loads of different elements. Some of the best dishes are the most straightforward.";
-      second.classList.add('para');
-  });
-    document.getElementById("box2").addEventListener('mouseout', function(){
-      second.innerHTML = box2Header;
-      second.classList.remove('para');
-  });
-  document.getElementById("box3").addEventListener('mouseover', function(){
-      third.innerHTML = "Mess up? Don't worry! It's usually salvageable - have a google and see what you can do - add lemon to counter spice, or water to counter salt. And if it's beyond help, you've learnt something - treat yourself to a takeaway!";
-      third.classList.add('para');
-  });
-   document.getElementById("box3").addEventListener('mouseout', function(){
-      third.innerHTML = box3Header;
-      third.classList.remove('para');
-   });
-  document.getElementById("box4").addEventListener('mouseover', function(){
-      fourth.innerHTML = "Cooking won't become a habit if you don't enjoy it, so don't make it into a source of stress for yourself. Everybody messes up and no one will judge you for it but yourself - so don't judge yourself, indulge all your most experimental whims, and give yourself a night off when you need it!";
-      fourth.classList.add('para');
-  });
-   document.getElementById("box4").addEventListener('mouseout', function(){
-      fourth.innerHTML =  box4Header;
-      fourth.classList.remove('para');
-   });
-   document.getElementById("box5").addEventListener('mouseover', function(){
-    fifth.innerHTML = "BOWLING CHALLENGE";
-    fifth.classList.add('para');
-});
- document.getElementById("box5").addEventListener('mouseout', function(){
-    fifth.innerHTML = box5Header;
-    fifth.classList.remove('para');
- });
+ 
+  $("#box1").mouseenter(function() {
+    $("#box1header").toggle();
+    $("#box1content").toggle();
+  })
+  $("#box1").mouseleave(function() {
+    $("#box1header").toggle();
+    $("#box1content").toggle();
+  })
+
+  $("#box2").mouseenter(function() {
+    $("#box2header").toggle();
+    $("#box2content").toggle();
+  })
+  $("#box2").mouseleave(function() {
+    $("#box2header").toggle();
+    $("#box2content").toggle();
+  })
+
+  $("#box3").mouseenter(function() {
+    $("#box3header").toggle();
+    $("#box3content").toggle();
+  })
+  $("#box3").mouseleave(function() {
+    $("#box3header").toggle();
+    $("#box3content").toggle();
+  })
+
+  $("#box4").mouseenter(function() {
+    $("#box4header").toggle();
+    $("#box4content").toggle();
+  })
+  $("#box4").mouseleave(function() {
+    $("#box4header").toggle();
+    $("#box4content").toggle();
+  })
+
+  $("#box5").mouseenter(function() {
+    $("#box5header").toggle();
+    $("#box5content").toggle();
+  })
+  $("#box5").mouseleave(function() {
+    $("#box5header").toggle();
+    $("#box5content").toggle();
+  })
+
+  $("#box6").mouseenter(function() {
+    $("#box6header").toggle();
+    $("#box6content").toggle();
+  })
+  $("#box6").mouseleave(function() {
+    $("#box6header").toggle();
+    $("#box6content").toggle();
+  })
+
 
